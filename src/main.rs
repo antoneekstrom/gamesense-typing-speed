8d6882438a6b00e84f108a678632abdcd7e6455d
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-05-19 19:36:22 +0200

    Initial commit!!!111

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+#![windows_subsystem = "windows"]
+
+mod typingspeed;
+use std::time::{Duration};
+
+use gamesense::{
+    client::GameSenseClient,
+    handler::screen::{self, ScreenHandler},
+};
+use serde_json::json;
+use trayicon::{MenuBuilder, TrayIcon, TrayIconBuilder};
+use typingspeed::TypingSpeedTracker;
+use winit::{event::{ElementState, Event, KeyboardInput}, event_loop::{ControlFlow, EventLoop, EventLoopProxy}};
+
+const GAME_NAME: &str = "TYPING_SPEED";
+const GAME_DISPLAY_NAME: &str = "Typing Speed Display";
+const DEVELOPER: &str = "Anton Ekström";
+const EVENT_NAME_SPEED: &str = "EVENT";
+const OLED_TIMEOUT_MILLIS: isize = 3000;
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+enum TrayIconEvent {
+    Exit
+}
+
+fn main() {
+    let mut tracker = TypingSpeedTracker::new(Duration::from_millis(OLED_TIMEOUT_MILLIS as u64));
+    let (mut client, screen_handler) = init_gamesense();
+
+    let event_loop = EventLoop::<TrayIconEvent>::with_user_event();
+    let proxy = event_loop.create_proxy();
+
+    let tray_icon = build_tray_icon(proxy);
+
+    start_client(&mut client, screen_handler);
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        // Move the tray_icon to the main loop (even if you don't use it)
+        //
+        // Tray icon uses normal message pump from winit, for orderly closure
+        // and removal of the tray icon when you exit it must be moved inside.
+        let _ = tray_icon;
+
+        match event {
+            Event::DeviceEvent { event, .. } => {
+                if let winit::event::DeviceEvent::Key(KeyboardInput { state, .. }) = event {
+                    if state == ElementState::Pressed {
+                        tracker.press_key();
+                        
+                        let timespan = Duration::from_secs(15);
+                        send_wpm(&mut client, tracker.typing_speed_within_timespan(timespan).wpm() as isize);
+                    }
+                }
+            }
+            Event::UserEvent(e) => match e {
+                TrayIconEvent::Exit => {
+                    *control_flow = ControlFlow::Exit;
+                    stop_client(&mut client);
+                }
+            },
+            _ => (),
+        }
+    });
+}
+
+fn build_tray_icon(proxy: EventLoopProxy<TrayIconEvent>) -> TrayIcon<TrayIconEvent> {
+    return TrayIconBuilder::new()
+        .sender_winit(proxy)
+        .icon_from_buffer(include_bytes!("../goblin.ico"))
+        .tooltip(GAME_DISPLAY_NAME)
+        .menu(MenuBuilder::new().item("Stop", TrayIconEvent::Exit))
+        .build()
+        .unwrap();
+}
+
+fn send_wpm(client: &mut GameSenseClient, wpm: isize) {
+    client
+        .trigger_event_frame(EVENT_NAME_SPEED, wpm, json!({ "wpm": wpm }))
+        .unwrap();
+}
+
+fn start_client(client: &mut GameSenseClient, handler: ScreenHandler) {
+    client
+        .bind_event(EVENT_NAME_SPEED, None, None, None, None, vec![handler])
+        .unwrap();
+    client.start_heartbeat();
+}
+
+fn stop_client(client: &mut GameSenseClient) {
+    client.stop_heartbeat().unwrap();
+}
+
+fn init_gamesense() -> (GameSenseClient, ScreenHandler) {
+    let client = GameSenseClient::new(GAME_NAME, GAME_DISPLAY_NAME, DEVELOPER, None).unwrap();
+
+    let handler = screen::ScreenHandler::new(
+        "screened",
+        "one",
+        screen::ScreenDataDefinition::StaticScreenDataDefinition(
+            screen::StaticScreenDataDefinition(vec![screen::ScreenFrameData::MultiLineFrameData(
+                screen::MultiLineFrameData {
+                    frame_modifiers_data: Some(screen::FrameModifiersData {
+                        length_millis: Some(OLED_TIMEOUT_MILLIS),
+                        icon_id: Some(screen::Icon::None),
+                        repeats: None,
+                    }),
+                    lines: vec![screen::LineData {
+                        type_options: screen::LineDataType::TextModifiersData(
+                            screen::TextModifiersData {
+                                has_text: true,
+                                prefix: None,
+                                suffix: Some(String::from(" WPM")),
+                                bold: None,
+                                wrap: None,
+                            },
+                        ),
+                        data_accessor_data: Some(screen::DataAccessorData {
+                            arg: None,
+                            context_frame_key: Some(String::from("wpm")),
+                        }),
+                    }],
+                },
+            )]),
+        ),
+    );
+
+    return (client, handler);
+}
---
+++ src/typingspeed.rs
@@ -0,0 +1,78 @@
+use std::time::{Duration, Instant};
+
+#[derive(Debug)]
+struct KeyPress {
+   pub time: Instant
+}
+pub struct TypingSpeedTracker {
+   key_presses: Vec<KeyPress>,
+   timeout: Duration
+}
+
+impl TypingSpeedTracker {
+
+   const MAX_KEY_PRESSES: usize = 700;
+
+   pub fn new(timeout: Duration) -> Self {
+      return TypingSpeedTracker {
+         key_presses: Vec::with_capacity(TypingSpeedTracker::MAX_KEY_PRESSES),
+         timeout
+      }
+   }
+
+   pub fn press_key(&mut self) {
+      let key_presses = &mut self.key_presses;
+
+      if key_presses.len() > 0 {
+         let last_key_press = &key_presses[0];
+         if last_key_press.time.elapsed() > self.timeout {
+            key_presses.clear();
+         }
+      }
+
+      key_presses.insert(0, KeyPress { time: Instant::now() });
+
+      if key_presses.len() >= TypingSpeedTracker::MAX_KEY_PRESSES {
+         key_presses.truncate(TypingSpeedTracker::MAX_KEY_PRESSES);
+      }
+   }
+
+   pub fn typing_speed_within_timespan(&self, timespan: Duration) -> TypingSpeed {
+      let keys_pressed = self.get_presses_within_timespan(timespan);
+      println!("keys_pressed: {}", keys_pressed.len());
+      return TypingSpeed::new(timespan, keys_pressed.len());
+   }
+
+   fn get_presses_within_timespan<'a>(&'a self, timespan: Duration) -> Vec<&'a KeyPress> {
+      return self.key_presses
+         .iter()
+         .filter(|key| key.time.elapsed() < timespan)
+         .collect();
+   }
+}
+
+pub struct TypingSpeed {
+   duration: Duration,
+   keys_pressed: usize
+}
+
+
+impl TypingSpeed {
+   pub const WPM_MODIFIER: f32 = 1.0 / 5.0;
+
+   pub fn new(duration: Duration, keys_pressed: usize) -> Self {
+      return TypingSpeed {
+         duration,
+         keys_pressed
+      }
+   }
+
+   pub fn wpm(&self) -> f32 {
+      return self.cpm() * TypingSpeed::WPM_MODIFIER;
+   }
+   
+   pub fn cpm(&self) -> f32 {
+      let TypingSpeed { duration, keys_pressed } = self;
+      return ((*keys_pressed as f32) / duration.as_secs_f32()) * 60.0;
+   }
+}
